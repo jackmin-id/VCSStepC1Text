74363b97a9831838674928baf43cdced615c243e
jackmin <minjwus@gmail.com>
2021-03-06 20:12:31 -0700

    first comit

---
+++ TextHello/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+            Console.WriteLine("another line");
+        }
+    }
+}
